7430e8aa2f8618fbf99797423bc469d2bf6b97a1
Morphius22 <peaceandhealth@protonmail.com>
2023-09-04 18:01:16 -0700

    finished adding in email and update flows

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const Videos = require("./models/videos");
+const nodemailer = require("nodemailer");
 
 //connect to mongoDB
 mongoose.set("strictQuery", false);
@@ -28,8 +29,18 @@ async function checkVideoStatus(videoId, apiKey) {
 
     if (data.items.length > 0) {
       console.log("Video exists:", data.items[0].snippet.title);
+      await Videos.updateOne(
+        { videoId: videoId.videoId },
+        { $set: { status: "Exists" } }
+      );
     } else {
       console.log("Video does not exist or is unavailable");
+      await Videos.updateOne(
+        { videoId: videoId.videoId },
+        { $set: { status: "No" } }
+      );
+      console.log("here is the videoid for email", videoId.videoId);
+      sendEmail(videoId.videoId);
     }
   } catch (error) {
     console.error("Error fetching video details:", error);
@@ -43,4 +54,32 @@ async function checkAllVideos() {
   }
 }
 
+async function sendEmail(videoId) {
+  // Create a transporter object using your email service's SMTP settings
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.username,
+      pass: process.env.password,
+    },
+  });
+
+  // Set up email options
+  const mailOptions = {
+    from: process.env.username,
+    to: process.env.username,
+    subject: "Content - Youtube Video Down",
+    text: `A youtube video in our content is not working. It has a video id of ${videoId}`,
+  };
+
+  // Send the email
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+}
 checkAllVideos();
